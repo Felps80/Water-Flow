7e0eba284c0cc4e2ae217eed027fb3e0c7fdf507
TynniBru <brunnorossi22@gmail.com>
2025-05-22 18:29:27 -0300

    Creditos Funcionais

--- Assets/Scripts/CreditScript.cs
+++ Assets/Scripts/CreditScript.cs
@@ -6,14 +6,22 @@ public class CreditScript : MonoBehaviour
 {
     public float scrollSpeed = 40f;
     private RectTransform rectTransform;
+    private Vector2 StartPosition;
+
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        StartPosition = rectTransform.anchoredPosition;
     }
 
     void Update()
-    {
+    {       
         rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+
+    public void StartCredits()
+    {
+        rectTransform.anchoredPosition = StartPosition;
     }
 
 }
--- Assets/Scripts/MainMenuControler.cs
+++ Assets/Scripts/MainMenuControler.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenuControler : MonoBehaviour
 {
-    public CreditScript creditsScript;
-
+    public GameObject TelaCreditos;
+    
     public void OnStartClick()
     {
         SceneManager.LoadScene("J2 Scene");
@@ -19,5 +20,7 @@ public class MainMenuControler : MonoBehaviour
 #endif
         Application.Quit();
     }
+
     
+
 }
